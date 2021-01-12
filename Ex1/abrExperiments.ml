8b8b42eee461449d839b87250aeb9d7595705dbd
Florian <florian-1992@hotmail.fr>
2021-01-12 20:53:18 +0100

    J'ai modifié certaines fonctions du 1.3 pour une meilleure complexité (mais ça reste lent quand il faut faire la moyenne de 1000 moyennes

--- Ex1/abrExperiments.ml
+++ Ex1/abrExperiments.ml
@@ -2,8 +2,8 @@
 #load "bst.cmo";;
 open Bst;;
 open Btree;;
-#show Bst;;
-#show Btree;;
+(* #show Bst;;
+#show Btree;; *)
 
 (* =========================================================================================== *)
 (* ======================================== Exercice 1 ======================================= *)
@@ -11,6 +11,8 @@ open Btree;;
 
 
 (* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ (1) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
+
+(* -> Main <- *)
 
 let bst_rnd_create(bound, treeSize : int * int) : int bst =
 
@@ -27,12 +29,20 @@ let bst_rnd_create(bound, treeSize : int * int) : int bst =
   !randABR
 ;;
 
-show_int_btree(bst_rnd_create(100, 10));;
+
+(* -> Test <- *)
+
+show_int_btree(bst_rnd_create(100, 20));;
 
 (* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
 
 
+
+
+(* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ (2) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
+
+(* -> Auxiliaries <- *)
 
 let isLeaf(tree: 'a t_btree) : bool =
   isEmpty(lson(tree)) && isEmpty(rson(tree))
@@ -59,12 +69,13 @@ let rec height(tree : 'a t_btree) : int =
     )
 ;;
 
+
+(* -> Main <- *)
+
 let desequilibre(tree : 'a t_btree) : int =
   height(lson(tree)) - height(rson(tree))
 ;;
 
-
-(* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ (2) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
 let avgDesequilibre(sampleSize, treeSize : int * int) : float =
 
@@ -77,58 +88,32 @@ let avgDesequilibre(sampleSize, treeSize : int * int) : float =
   !sum /. float_of_int(sampleSize)
 ;;
 
+let avgAvgDesequilibre(nbAvgs, sampleSize, treeSize : int * int * int) : float =
+  
+  let sum : float ref = ref 0. in
+
+  for i=1 to nbAvgs do
+    sum := !sum +. avgDesequilibre(sampleSize, treeSize);
+  done;
+
+  !sum /. float_of_int(nbAvgs)
+;;
+
+
+(* -> Test <- *)
+
 avgDesequilibre(1000, 10);;
+avgAvgDesequilibre(100, 100, 100);;
 
 (* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
 
 
 
-let rec size(tree : 'a t_btree) : int =
-
-  if(isEmpty(tree))
-  then 0
-
-  else
-    (
-      let l : 'a t_btree = lson(tree) in
-      let r : 'a t_btree = rson(tree) in
-      1 + size(l) + size(r);
-    )
-;;
 
 (* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ (3) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
-let createSeries(len : int) : int list =
-
-  Random.self_init();
-
-  if(len<=0)
-  then
-    (
-      let randLowerBound : int = Random.int 1000 in
-      let res : int list ref = ref [] in
-
-      for i=1 to Random.int 100 do
-        res := (randLowerBound+(i-1))::!res
-      done;
-
-      !res
-    )
-
-  else
-    (
-      let randLowerBound : int = Random.int 1000 in
-      let res : int list ref = ref [] in
-
-      for i=1 to len do
-        res := (randLowerBound+(i-1))::!res
-      done;
-
-      !res
-    )
-;;
-
+(* -> Auxiliaries <- *)
 
 let bst_rndSeries_create(treeSize, seriesLen : int * int) : int bst =
 
@@ -136,23 +121,28 @@ let bst_rndSeries_create(treeSize, seriesLen : int * int) : int bst =
 
   let empty_tree : int bst = empty() in
   let randABR : int bst ref = ref empty_tree in
-  let deltaSizes : int ref = ref treeSize in
+  let fillerCount : int ref = ref treeSize in
 
-  while(!deltaSizes != 0) do
-    let rndSeries : int list = createSeries(seriesLen) in
+  while(!fillerCount > 0) do
+    let randLowerBound : int = Random.int 1001 in
+    let len : int = if(seriesLen<=0) then Random.int 101 else seriesLen in
 
-    for i=0 to List.length(rndSeries)-1 do
-      if(!deltaSizes > 0)
+    for i=1 to len do
+      if(!fillerCount > 0)
       then (
-        randABR := bst_linsert(!randABR, List.nth rndSeries i);
-        deltaSizes := !deltaSizes - 1;
+        randABR := bst_linsert(!randABR, randLowerBound+(i-1));
+        fillerCount := !fillerCount - 1;
       )
     done;
-
+    
   done;
-
+  
   !randABR
 ;;
+
+
+
+(* -> Main <- *)
 
 let avgSeriesDesequilibre(sampleSize, treeSize, seriesLenMode : int * int * char) : float =
 
@@ -162,7 +152,7 @@ let avgSeriesDesequilibre(sampleSize, treeSize, seriesLenMode : int * int * char
     match seriesLenMode with
     | 'r' -> sum := !sum +. float_of_int(desequilibre(bst_rndSeries_create(treeSize, -1)))
     | 'f' -> sum := !sum +. float_of_int(desequilibre(bst_rndSeries_create(treeSize, 10)))
-    | 'a' -> sum := !sum +. float_of_int(desequilibre(bst_rndSeries_create(treeSize, 1+i)))
+    | 'a' -> sum := !sum +. float_of_int(desequilibre(bst_rndSeries_create(treeSize, i)))
     | 'd' -> sum := !sum +. float_of_int(desequilibre(bst_rndSeries_create(treeSize, sampleSize-i)))
     | _ -> failwith("Wrong mode for series length... 'r' for random lengths, 'f' for fixed lengths, 'a' for ascending lengths, and 'd' for descending lengths."); 
   done;
@@ -170,14 +160,33 @@ let avgSeriesDesequilibre(sampleSize, treeSize, seriesLenMode : int * int * char
   !sum /. float_of_int(sampleSize)
 ;;
 
+let avgAvgSeriesDesequilibre(nbAvgs, sampleSize, treeSize, seriesLenMode : int * int * int * char) : float =
 
-(*show_int_btree(bst_rndSeries_create(100, 100));;
-desequilibre(bst_rndSeries_create(10, 1));;*)
-avgSeriesDesequilibre(1000, 100, 'r');;
-avgSeriesDesequilibre(1000, 100, 'f');;
-avgSeriesDesequilibre(1000, 100, 'a');;
-avgSeriesDesequilibre(1000, 100, 'd');;
-avgSeriesDesequilibre(1000, 100, 'e');;
+  let sum : float ref = ref 0. in
+
+  for i=1 to nbAvgs do
+    sum := !sum +. avgSeriesDesequilibre(sampleSize, treeSize, seriesLenMode)
+  done;
+
+  !sum /. float_of_int(nbAvgs)
+;;
+
+
+(* -> Test <- *)
+
+(* show_int_btree(bst_rndSeries_create(20, 3));;*) 
+(* desequilibre(bst_rndSeries_create(10, 1));; *)
+
+(*avgSeriesDesequilibre(1000, 100, 'r');;
+avgSeriesDesequilibre(100, 100, 'f');;
+avgSeriesDesequilibre(100, 100, 'a');;
+avgSeriesDesequilibre(100, 100, 'd');;
+avgSeriesDesequilibre(100, 100, 'e');;*)
+
+avgAvgSeriesDesequilibre(100, 100, 100, 'r');;
+avgAvgSeriesDesequilibre(100, 100, 100, 'f');;
+avgAvgSeriesDesequilibre(100, 100, 100, 'a');;
+avgAvgSeriesDesequilibre(100, 100, 100, 'd');;
 (* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
 
@@ -187,7 +196,22 @@ avgSeriesDesequilibre(1000, 100, 'e');;
 
 (* 
    Compte-rendu d'expérimentation : 
-   https://fr.overleaf.com/project/5ffc59b4685334742908ab9d 
+   https://fr.overleaf.com/read/jsvdspcwzjjd
  *)
 
 (* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
+
+
+(* 
+   QUESTIONS POUR L'ENSEIGNANT:
+   ----------------------------
+
+   1) Moyenne de moyennes?
+   2) ABR avec doublons ou sans?
+   3) Valeurs attendues (notamment moyennes souvent positives dans le 
+      cas de random) ?
+   4) Récursivité ou itérativité pour les fonctions?
+   5) Fonctions auxiliaires dans des modules ou ici?
+   6) Noms de fonctions (cf. avgAvg...) ?
+   7) Complexité des fonctions d'expérimentations prises en compte?
+ *)
